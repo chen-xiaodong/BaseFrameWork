aca17e77671495dba4dd1c9280c4bc61a215515b
chenxiaodong <1346650911@qq.com>
2020-09-29 16:51:55 +0800

    整体流程完善

--- Test/test/test_method.py
+++ Test/test/test_method.py
@@ -1,3 +1,4 @@
+#! /usr/bin/env python3
 """
 
 @FileName: test_method.py
@@ -6,40 +7,18 @@
 @Descriptions: 
 
 """
-import pytest
+from tools.tools import *
 
-from basic_tool.adb import adb
-from basic_tool.excel import excel_tools
-from basic_tool.sql import sql_tools
-
-#
-# def test_adb():
-#     adb_tool = adb().adb_start_server()
-#     print(adb_tool)
-#
-#
-# def test_db():
-#     db = sql_tools("localhost", "root", "123456", "test", port=3306)
-#     db.connect_mysql()
-#     print(db.deal_sql("select * from user "))self.workbook.sheet_by_index(sheet)
-
-
-# def test_excel():
-#     excel = excel_tools("../../test3.xls")
-#     excel.open_excel()
-#     excel.change_sheet(2)
-#     sheet = excel.get_row_values(2)
-#     print(sheet)
-from basic_tool.yaml import yaml_tools
-from drivers.selenium import selenium_driver
-
-
-def test_driver():
-    # driver = selenium_driver("86.0.4240.22")
-    # driver.get_webdriver_package()
-    yaml = yaml_tools("../../drivers/driver_download.yaml")
-    print(yaml.yaml_to_dict())
-
-
-if __name__ == "__main__":
-    test_driver()
+config_file = Yaml(path="config/config.yaml")
+file = config_file.load()
+appium_config = config_file.get_config_by_path("appium_config")
+devices = config_file.get_config_by_path("device_ip2")
+print(devices)
+adb = Adb(devices)
+adb.connect()
+app = Appium(desc=appium_config)
+# app.start_server()
+driver = app.start_driver()
+case_list = ["case2"]
+cases = CaseLoader(config_file, case_list, driver=driver)
+cases.load(10)
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="wasu_test",
-    version="1.4",
+    version="1.9.1",
     author="XiaoDong Chen",
     author_email="chenxiaodong@wasu.com",
     description="The Wasu Auto TestFrameWork",
@@ -14,7 +14,9 @@ setuptools.setup(
     url="",
     packages=setuptools.find_packages(),
     install_requires=['PyYAML>=5.3', 'PyMySql>=0.10.1',
-                      'xlrd>=1.2.0', 'requests>=2.24.0'],
+                      'xlrd>=1.2.0', 'requests==2.24.0',
+                      'selenium==3.141.0', 'python-benedict==0.19.0', 'Appium-Python-Client~=1.0.2',
+                      ],
     entry_points={
 
     },
